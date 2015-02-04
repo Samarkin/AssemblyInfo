bdbd324a7878ff910eb328b4638ce92323fd2855
Pavel Samarkin <samarkinpa@gmail.com>
2015-02-03 22:23:30 -0800

    AssemblyInfo.Common embedded into the WPF application

--- AssemblyInfo/App.xaml.cs
+++ AssemblyInfo/App.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
 
 namespace AssemblyInfo
@@ -13,5 +9,19 @@ namespace AssemblyInfo
 	/// </summary>
 	public partial class App : Application
 	{
+		public App()
+		{
+			AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+			{
+				String resourceName = Assembly.GetExecutingAssembly().GetName().Name + "." + new AssemblyName(args.Name).Name + ".dll";
+				using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+				{
+					if (stream == null) return null;
+					Byte[] assemblyData = new Byte[stream.Length];
+					stream.Read(assemblyData, 0, assemblyData.Length);
+					return Assembly.Load(assemblyData);
+				}
+			};
+		}
 	}
 }
